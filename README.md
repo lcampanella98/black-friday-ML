80a20867e1367334a493a449085c3796b5c7e309
Lorenzo Campanella <enzocampanella98@gmail.com>
2019-04-25 11:00:44 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # black-friday-ML
 Black Friday project for CS301
+
+Data:
+https://www.kaggle.com/mehdidag/black-friday/version/1
---
+++ main.py
@@ -0,0 +1,6 @@
+"""
+CS 301 Intro to Data Science Project
+Project: Black Friday
+Group: Nikhileshwarananda Vummadi, Lorenzo Campanella
+Date: 4/25/2019
+"""
